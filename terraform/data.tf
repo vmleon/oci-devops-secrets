d27206382b0a3bcee7cb65c3b8ee34f858ff16cd
Victor Martin <vmleon@gmail.com>
2023-07-03 10:27:57 +0200

    fix missing ADs data on terraform

--- terraform/data.tf
+++ terraform/data.tf
@@ -16,3 +16,7 @@ data "oci_identity_regions" "home_region" {
 data "oci_identity_compartment" "compartment" {
   id = var.compartment_ocid
 }
+
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.tenancy_ocid
+}
